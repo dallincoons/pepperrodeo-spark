f96e8fe246a11db13bd72dff9b170eae7bc46a36
dallin <dallinis@hotmail.com>
2016-11-14 10:15:42 -0700

    larger screen home styling

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -5,7 +5,7 @@
 
         <li  class="l-nav-li"><a href="/recipe" class="{{ set_active_strict('recipe') }}"><i class="fa fa-cutlery"></i> My Recipes</a>
             <div class="l-dropdown-content">
-                <a href="/recipe/create" class="{{ set_active_strict('recipe/create') }}"><i class="fa fa-plus"></i> Create New Recipe</a>
+                <a href="/recipe/create" class="{{ set_active_strict('recipe/create') }}"><i class="fa fa-plus"></i> Add a Recipe</a>
                 <a><i class="fa fa-cart-plus"></i> Add Recipes to List</a>
                 <a href="/recipe"><i class="fa fa-trash"></i> Delete Recipes</a>
             </div>
