294a676b4226d698bb1e140910595e965ddd84e8
dallin <dallinis@hotmail.com>
2016-11-19 13:02:11 -0700

    styling for single list page

--- resources/views/grocerylists/single-grocery-list.blade.php
+++ resources/views/grocerylists/single-grocery-list.blade.php
@@ -2,15 +2,14 @@
 
 @section('content')
 <single-list inline-template>
-<div>
-    <div class="grocery-list-wrapper">
+<div class="grocery-list-wrapper">
 
-        <h2 class="page-title">{{$grocerylist->title}}</h2>
+        <h2 class="page-title">{{$grocerylist->title}} <a href="/grocerylist/{{$grocerylist->id}}/edit" class="list-edit-large"><i class="fa fa-pencil"></i></a></h2>
 
         <nav class="mini-nav">
-            <ul class="list-nav">
+            <ul class="mini-nav-options">
                 <li><a href="/grocerylist/{{$grocerylist->id}}/edit"><i class="fa fa-pencil"></i></a></li>
-                <li><form action="/grocerylist/{{$grocerylist->id}}" method="POST" id="list-delete">
+                <li><form action="/grocerylist/{{$grocerylist->id}}" method="POST" id="list-delete" class="mini-delete">
                         <input type="hidden" name="_method" value="DELETE">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <a v-on:click="submitDeleteList()"><i class="fa fa-trash"></i></a>
@@ -19,8 +18,8 @@
         </nav>
 
         <div class="list-view-toggle">
-            <a href="/grocerylist/{{$grocerylist->getKey()}}?sortBy=item" class="toggle-active">By Items</a>
-            <a href="/grocerylist/{{$grocerylist->getKey()}}?sortBy=recipe" class="toggle-inactive">By Recipe</a>
+            <a href="/grocerylist/{{$grocerylist->getKey()}}?sortBy=item" class="toggle toggle-active">By Dept.</a>
+            <a href="/grocerylist/{{$grocerylist->getKey()}}?sortBy=recipe" class="toggle toggle-inactive">By Recipe</a>
         </div>
 
         @foreach($grocerylist->items as $category => $list_items)
@@ -28,16 +27,16 @@
                 <ul class="category">
                     <li class="category-title"><h3>{{$category ?: 'N/A'}}</h3></li>
                     <li>
-                        <ul class="recipes list-items">
+                        <ul class="list-items">
                             @foreach($list_items as $item)
-                                <li>{{$item->quantity}} {{$item->type}} {{$item->name}}</li>
+                                <li class="list-item">{{$item->quantity}} {{$item->type}} {{$item->name}}</li>
                             @endforeach
                         </ul>
                     </li>
                 </ul>
             </div>
         @endforeach
-    </div>
+
 </div>
 </single-list>
 @endsection
