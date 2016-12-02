7f599eb994a25949a794f473148ece584f851f98
dallin <dallinis@hotmail.com>
2016-12-02 10:23:56 -0700

    recipe category pages styling for all screens

--- resources/views/recipe_categories/show.blade.php
+++ resources/views/recipe_categories/show.blade.php
@@ -1,19 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2><a href="/recipe">All Recipes</a></h2>
-    <div class="category-wrapper">
-        <ul class="category">
-            <li class="category-title"><h3>{{$category->name}}</h3></li>
-            <li>
-                <ul class="recipes">
-                    @foreach($category->recipes as $recipe)
-                        <li>
-                            <label class="control control--checkbox"><a href="/recipe/{{$recipe->id}}">{{$recipe->title}}</a></label>
-                        </li>
-                    @endforeach
-                </ul>
-            </li>
-        </ul>
+
+    <div class="content-wrapper show-cat-wrapper">
+
+        <div class="category-wrapper">
+            <ul class="category">
+                <li class="category-title"><h3>{{$category->name}}</h3></li>
+                <li>
+                    <ul class="recipes">
+                        @foreach($category->recipes as $recipe)
+                            <li class="recipe">
+                                <label class="control control--checkbox"><a href="/recipe/{{$recipe->id}}">{{$recipe->title}}</a></label>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="all-recipes-btn"><a href="/recipe"><i class="fa fa-arrow-circle-o-left"></i> All Recipes</a></div>
     </div>
+
 @endsection
