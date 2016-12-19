181857a22ea64d272c7401a192b146682be8cb4f
Dallin <dallinis@hotmail.com>
2016-12-19 13:22:48 -0700

    styling for 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
-    Uh oh, you upset the balance
+    <div class="content-wrapper">
 
+        <h2 class="page-title">Ooops, something went wrong.</h2>
+
+        <div class="error-img-wrapper">
+            <iframe src="//giphy.com/embed/hbd8nlok7kqnS?html5=true" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+            <h3>Try going <a href="/" class="error-link">home</a> or <a class="error-link">contact us</a></h3>
+        </div>
+
+
+
+    </div>
 @endsection
