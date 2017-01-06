9688da43af01af408cc0e9cc7cb26ee0dd75ccb2
Dallin <dallinis@hotmail.com>
2017-01-06 11:29:32 -0700

    first draft of changes to home page styling

--- resources/views/home/includes/user-home.blade.php
+++ resources/views/home/includes/user-home.blade.php
@@ -1,22 +1,35 @@
 <div class="content">
     <div class="section">
-            <a href="/recipe/create" class="home-option">+ <i class="fa fa-cutlery fa-fw"> </i>  Add a Recipe</a>
+        <div class="home-text">
+            <a href="/recipe/create" class="home-option">Add a Recipe</a>
+        </div>
+
+        <div class="home-background">
+            <i class="fa fa-cutlery"></i>
+        </div>
     </div>
 
     <div class="section">
-            <a href="/grocerylist/create" class="home-option">+ <i class="fa fa-shopping-cart fa-fw"> </i>  Create a List</a>
+        <div class="home-text">
+            <a href="/grocerylist/create" class="home-option">Create a List</a>
+        </div>
+
+        <div class="home-background">
+            <i class="fa fa-shopping-cart"></i>
+        </div>
+
     </div>
 
     <div class="divider"></div>
 
-    <div class="section">
-        <h2>Recent Lists</h2>
-        @foreach($lists as $list)
-            <h3><i class="fa fa-list"></i> <a href="grocerylist/{{$list->getKey()}}">{{$list->title}}</a></h3>
-        @endforeach
+    {{--<div class="section">--}}
+        {{--<h2>Recent Lists</h2>--}}
+        {{--@foreach($lists as $list)--}}
+            {{--<h3><i class="fa fa-list"></i> <a href="grocerylist/{{$list->getKey()}}">{{$list->title}}</a></h3>--}}
+        {{--@endforeach--}}
 
-        <a href="/grocerylist" class="more">All Lists</a>
-    </div>
+        {{--<a href="/grocerylist" class="more">All Lists</a>--}}
+    {{--</div>--}}
 </div>
 
 
