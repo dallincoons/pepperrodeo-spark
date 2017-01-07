03c12b246c1218a91728d58c76cc6b165dd6a5d4
Dallin <dallinis@hotmail.com>
2017-01-07 11:32:44 -0700

    added styling for toggle list options dropdown

--- resources/views/grocerylists/includes/list-form.blade.php
+++ resources/views/grocerylists/includes/list-form.blade.php
@@ -50,14 +50,16 @@
                     <span class="list-item-added">@{{ item.quantity }}</span>
                     <span class="list-item-added">@{{ item.type }}</span>
                     <span class="list-item-added">@{{ item.name }} </span>
+
                 </div>
                 <div class="options-dropdown-wrapper">
-                    <a class="dropdown-indicator" v-on:click="toggleListOptions(item)" >...</a>
+                    <a class="dropdown-indicator" v-on:click="toggleListOptions(item)" ><i class="fa fa-ellipsis-h"></i></a>
                     <ul class="options-dropdown" v-show="item.toggleOptions">
                         <li><i class="fa fa-pencil"></i><a> Edit Item</a></li>
                         <li><i class="fa fa-trash-o"></i><a> Delete Item</a></li>
                     </ul>
                 </div>
+
 
                 <input type="hidden" :name="'items[' + item.id + '][quantity]'" :value="item.quantity">
                 <input type="hidden" :name="'items[' + item.id + '][name]'" :value="item.name">
